ef6d504a9002fcfead91aa5f0f35873d0dd64ff6
LeoCR <leonardoas2007@gmail.com>
2020-07-27 16:43:23 -0600

    Updating a Project

--- BackEnd/ppmtool/src/main/java/io/laranibar/ppmtool/web/ProjectController.java
+++ BackEnd/ppmtool/src/main/java/io/laranibar/ppmtool/web/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +58,17 @@ public class ProjectController {
 	public ResponseEntity<?> deleteProject(@PathVariable String projectId){
 		projectService.deleteProjectByIdentifier(projectId);
 		return new ResponseEntity<String>("Project with ID:"+projectId+" was deleted succesfully.",HttpStatus.OK);
+	}
+	@PutMapping("/update")
+	public ResponseEntity<?> updateProject(@Valid @RequestBody Project project,BindingResult result){
+		
+		ResponseEntity<?> errorMap=mapValidationErrorService.MapValidationService(result);
+		
+		if(errorMap!=null) {
+			return errorMap;
+		}
+		
+		Project project1= projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project1,HttpStatus.OK);
 	}
 }
