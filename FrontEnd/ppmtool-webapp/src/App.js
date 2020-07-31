00df824691fa9a368adb6aad424c19e9ac58ed5b
LeoCR <leonardoas2007@gmail.com>
2020-07-30 18:23:48 -0600

    Installing React and Redux

---
+++ FrontEnd/ppmtool-webapp/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import './App.css';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router,Route,Switch } from "react-router-dom"
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header/> 
+        <Switch>
+            <Route path="/" exact component={Dashboard}/>
+            <Route path="/dashboard" exact component={Dashboard}/>
+            <Route path="/add-project" exact component={AddProject}/>
+        </Switch> 
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ FrontEnd/ppmtool-webapp/src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initalState = {};
+const middleware = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    rootReducer,
+    initalState,
+    composeWithDevTools(
+      applyMiddleware(...middleware)
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(applyMiddleware(...middleware))
+  );
+}
+
+export default store;
