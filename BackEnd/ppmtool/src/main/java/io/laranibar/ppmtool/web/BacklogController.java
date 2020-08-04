ec04e255b9f0809c082a8b8425aa74fdc7163d48
LeoCR <leonardoas2007@gmail.com>
2020-08-04 09:54:26 -0600

    Finding Project Task by Project Sequence

--- BackEnd/ppmtool/src/main/java/io/laranibar/ppmtool/web/BacklogController.java
+++ BackEnd/ppmtool/src/main/java/io/laranibar/ppmtool/web/BacklogController.java
@@ -45,4 +45,12 @@ public class BacklogController {
     	
     }
 
+    @GetMapping("/getProjectTask/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id,@PathVariable String pt_id){
+		
+    	ProjectTask projectTask=projectTaskService.findPTByProjectSequence(backlog_id,pt_id);
+    	
+    	return new ResponseEntity<ProjectTask>(projectTask,HttpStatus.OK);
+    	
+    }
 }
